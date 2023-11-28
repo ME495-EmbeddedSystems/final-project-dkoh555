9716f15b43646002223f1f809554fea68d30b9a3
Henry Buron <henryburon34@gmail.com>
2023-11-28 16:03:09 -0600

    small format change

--- speech/speech/listen_speech.py
+++ speech/speech/listen_speech.py
@@ -8,10 +8,11 @@ Github: https://github.com/Uberi/speech_recognition/tree/master
 Documentation: https://pypi.org/project/SpeechRecognition/
 """
 
+
 class State(Enum):
     WAITING = auto()
     LISTENING = auto()
-
+    RECOGNIZING = auto()
 
 class ListenSpeech(Node):
     """Listens to speech and publishes string"""
@@ -20,33 +21,45 @@ class ListenSpeech(Node):
         super().__init__("listen_speech")
         self.state = State.WAITING
 
-        self.timer = self.create_timer(1.0 / 100.0, self.timer_callback)
+        self.timer = self.create_timer(1.0/100.0, self.timer_callback)
 
         # Create a recognizer instance...
         self.recognizer = sr.Recognizer()
+        self.audio = None
+        self.spoken_language = None
+        self.text = None
 
     
     def timer_callback(self):
 
         if self.state == State.LISTENING:
+
             # Currently using default microphone (from computer) as audio source
             with sr.Microphone() as source:
                 print("Say something...")
                 # Adjust for ambient noise (if necessary)
                 self.recognizer.adjust_for_ambient_noise(source)
                 # Listen for speech (by default, it listens until it detects a pause)
-                audio = self.recognizer.listen(source)
+                self.audio = self.recognizer.listen(source)
 
+            self.state = State.WAITING
+
+        elif self.state == State.RECOGNIZING:
+
+            # Turn the recorded language into a string
             try:
-                lang = 'en'
-                text = self.recognizer.recognize_google(audio, language=lang)
-                print("You said:", text)
+                self.spoken_language = 'en'
+                self.text = self.recognizer.recognize_google(self.audio, language=self.spoken_language)
+                print("You said:", self.text)
             except sr.UnknownValueError:
                 print("Sorry, could not understand audio")
             except sr.RequestError as e:
                 print("Error:", str(e))
             
             self.state = State.WAITING
+
+# I think next I need to publish the speech onto a topic
+# In addition, need to figure out how to get name of the language that is being spoken in
 
 def listen_speech_entry(args=None):
     rclpy.init(args=args)
